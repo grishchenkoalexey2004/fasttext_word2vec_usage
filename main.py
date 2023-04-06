19f24c59ef066661d5fdd0624530ab69b4d8e46f
Алексей Грищенко <grishchenkoalexey2004@yandex.ru>
2023-04-06 13:00:33 +0300

    add: added Speerman correlation calculation and wordsim files

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import io
-import fasttext
+from numpy import dot
+from numpy.linalg import norm
+from scipy.stats import spearmanr
 
 
-vect_file = "crawl-300d-2M.vec"
+
+vect_file = "crawl-300d-2M-subword.vec"
 wsim_rel_file = "wordsim_relatedness_goldstandard.txt"
 wsim_sim_file = "wordsim_similarity_goldstandard.txt"
 
 
 alphabet = {} #set of unique words in both files with their vectors extracted from 
 
-
-#loads word pairs and forms list of unique words
+#fills alphabet dict
+#returns matrix of pairs extracted from text file 
+# each array in matrix contains 3 elements
+# word1 word2 relatedness_value
 def load_word_pairs(fname):
     global alphabet
     pairs = [] 
@@ -35,6 +40,31 @@ def load_vectors(fname):
         if (alphabet.get(word)!=None):
             tokens = line.rstrip().split(' ') #word + vector float values
             alphabet[word] = map(float, tokens[1:])
+    for key in alphabet:
+        alphabet[key] = list(alphabet[key])
+
+#calculates cos of the angle between 2 float vectors
+def calc_cos(vec1,vec2):
+    scal_prod = dot(vec1,vec2)
+    return scal_prod/(norm(vec1)*norm(vec2))
+
+
+#calculates cosine relatedness for all pairs in the array, and adds calculated result to the array 
+def add_fasttext_relatedness(pair_matr):
+    for i in range(len(pair_matr)):
+        vec1 = alphabet[pair_matr[i][0]]
+        vec2 = alphabet[pair_matr[i][1]]
+        relatedness = calc_cos(vec1,vec2)
+        pair_matr[i].append(relatedness)
+
+
+# each array describing a pair contains 4 elems
+# word1 word2 relatedness from the file, cosine relatedness from wordtext
+def calc_spearman(pair_matr):
+    wordsim_rel_arr = list(map(lambda elem:elem[2],pair_matr))
+    fasttext_rel_arr = list(map(lambda elem:elem[3],pair_matr))
+    result,p_val = spearmanr(wordsim_rel_arr,fasttext_rel_arr)
+    return result
 
 wsim_rel_pairs = load_word_pairs(wsim_rel_file)
 wsim_sim_pairs = load_word_pairs(wsim_sim_file)
@@ -42,8 +72,12 @@ wsim_sim_pairs = load_word_pairs(wsim_sim_file)
 
 load_vectors(vect_file)
 
-for key in alphabet:
-    alphabet[key] = list(alphabet[key])
+add_fasttext_relatedness(wsim_rel_pairs) 
+add_fasttext_relatedness(wsim_sim_pairs)
 
-print(alphabet["tiger"])
+result1 = calc_spearman(wsim_rel_pairs) #spearman correlation betw rel values from wsim_relatedness and cosine rel values 
+result2 = calc_spearman(wsim_sim_pairs) #spearman correlation betw rel values from wsim_similarity and ..
+
+print("spearman correlation betw rel values from wsim_relatedness and cosine rel values : {}".format(result1))
+print("spearman correlation betw rel values from wsim_similarity and cosine rel values : {}".format(result2))
 
